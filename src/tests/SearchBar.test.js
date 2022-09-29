185d4e952c910df3a58094b8523aba79db75d558
Bruno Marques <brunorcmarques@gmail.com>
2022-09-29 17:03:29 -0300

    100% cobertura testes do searchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helper/renderWithRouter';
 import allMeals from './mocks/allMeals.json';
+import oneMeal from '../../cypress/mocks/oneMeal';
+import oneDrink from '../../cypress/mocks/oneDrink';
 import Provider from '../context/Provider';
 import Meals from '../pages/Meals';
 import Drinks from '../pages/Drinks';
+import App from '../App';
 
 const EXEC_SEARCH_BTN = 'exec-search-btn';
 const SEARCH_TOP_BTN = 'search-top-btn';
@@ -72,44 +75,47 @@ describe('Search Bar tests', () => {
     expect(global.alert).toHaveBeenCalledTimes(1);
   });
 
-  it.only('The food recipe detail is exhibited when the search return a single recipe', async () => {
-    const { history } = renderWithRouter(<Provider><Meals /></Provider>, '/meals');
+  it('The food recipe detail is exhibited when the search return a single recipe', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(oneMeal),
+    });
+
+    const { history } = renderWithRouter(<App />, '/meals');
 
     const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
     userEvent.click(searchBtn);
 
     const searchInput = screen.getByTestId(SEARCH_INPUT);
     const executeSearchButton = screen.getByTestId(EXEC_SEARCH_BTN);
-    const ingredientSearchRadio = screen.getByTestId(INGREDIENT_RADIO);
+    const nameSearchRadio = screen.getByTestId(NAME_RADIO);
 
-    userEvent.click(ingredientSearchRadio);
-    userEvent.type(searchInput, 'Filo Pastry');
+    userEvent.click(nameSearchRadio);
+    userEvent.type(searchInput, 'Arrabiata');
     userEvent.click(executeSearchButton);
 
-    const loading = screen.getByText('Loading');
-    expect(loading).toBeInTheDocument();
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/meals/52977');
+    await waitFor(() => expect(history.location.pathname).toBe('/meals/52771'));
   });
 
-  // it('The drink recipe detail is exhibited when the search return a single recipe', async () => {
-  //   const { history } = renderWithRouter(<Provider><Drinks /></Provider>, '/drinks');
+  it('The drink recipe detail is exhibited when the search return a single recipe', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(oneDrink),
+    });
 
-  //   const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
-  //   userEvent.click(searchBtn);
+    const { history } = renderWithRouter(<App />, '/drinks');
 
-  //   const searchInput = screen.getByTestId(SEARCH_INPUT);
-  //   const executeSearchButton = screen.getByTestId(EXEC_SEARCH_BTN);
-  //   const nameSearchRadio = screen.getByTestId(NAME_RADIO);
+    const searchBtn = screen.getByTestId(SEARCH_TOP_BTN);
+    userEvent.click(searchBtn);
 
-  //   userEvent.click(nameSearchRadio);
-  //   userEvent.type(searchInput, 'Gin');
-  //   userEvent.click(executeSearchButton);
+    const searchInput = screen.getByTestId(SEARCH_INPUT);
+    const executeSearchButton = screen.getByTestId(EXEC_SEARCH_BTN);
+    const nameSearchRadio = screen.getByTestId(NAME_RADIO);
 
-  //   const { pathname } = history.location;
-  //   expect(pathname).toBe('/drinks');
-  // });
+    userEvent.click(nameSearchRadio);
+    userEvent.type(searchInput, 'Gin');
+    userEvent.click(executeSearchButton);
+
+    await waitFor(() => expect(history.location.pathname).toBe('/drinks/178319'));
+  });
 
   it('Test search food recipe by ingredient', async () => {
     renderWithRouter(<Provider><Meals /></Provider>, '/meals');
