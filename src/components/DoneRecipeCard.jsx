3adbffffed26d9e4e1552df219a05b6222669537
Marco <marcobotelho93@gmail.com>
2022-09-29 18:30:48 -0300

    Requisito 44 done

---
+++ src/components/DoneRecipeCard.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+
+function DoneRecipeCard({ param, index }) {
+  const { name, nationality, tags, image, doneDate } = param;
+
+  return (
+    <div>
+      <div id="meal-recipe-card">
+        <div data-testid={ `${index}-recipe-card` }>
+          <div>
+            <img
+              src={ image }
+              alt={ name }
+              data-testid={ `${index}-horizontal-image` }
+              style={ { width: '150px' } }
+              // Tamanho da imagem limitado
+            />
+            <p data-testid={ `${index}-horizontal-name` }>
+              { name }
+            </p>
+            <p data-testid={ `${index}-horizontal-top-text` }>
+
+              { nationality}
+            </p>
+            <p data-testid={ `${index}-horizontal-done-date` }>
+              { doneDate }
+            </p>
+          </div>
+          <button
+            data-testid={ `${index}-horizontal-share-btn` }
+            type="button"
+            className="share-button"
+          >
+            <img src={ shareIcon } alt="share button" />
+          </button>
+
+          <div>
+            { tags.map((tag) => (
+              <p
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                {tag}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  param: PropTypes.objectOf(PropTypes.string).isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default DoneRecipeCard;
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,52 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
 function RecipeCard({ param, index }) {
   // function RecipeCard({ param }, { children }) {
   const { strMeal, strMealThumb, strDrink, strDrinkThumb } = param;
 
-  const { pathname } = useLocation();
+  // const { pathname } = useLocation();
 
   return (
     <div>
       <div id="meal-recipe-card">
-        { pathname === '/meals'
-          ? (
-            <div data-testid={ `${index}-recipe-card` }>
-              <div>
-                <img
-                  src={ strMealThumb }
-                  alt={ strMeal }
-                  data-testid={ `${index}-card-img` }
-                  style={ { width: '150px' } }
-                  // Tamanho da imagem limitado
-                />
-                <p data-testid={ `${index}-card-name` }>
-                  { strMeal }
-                </p>
-              </div>
-            </div>)
-          : null}
-      </div>
-      <div id="drinks-div">
-        { pathname === '/drinks'
-          ? (
-            <div>
-              <div data-testid={ `${index}-recipe-card` }>
-                <img
-                  src={ strDrinkThumb }
-                  alt={ strDrink }
-                  data-testid={ `${index}-card-img` }
-                  style={ { width: '150px' } }
-                  // Tamanho da imagem limitado durante desenvolvimento
-                />
-                <p data-testid={ `${index}-card-name` }>
-                  { strDrink }
-                </p>
-              </div>
-            </div>)
-          : null}
+        <div data-testid={ `${index}-recipe-card` }>
+          <div>
+            <img
+              src={ strMealThumb !== undefined ? strMealThumb : strDrinkThumb }
+              alt={ strMeal !== undefined ? strMeal : strDrink }
+              data-testid={ `${index}-card-img` }
+              style={ { width: '150px' } }
+              // Tamanho da imagem limitado
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { strMeal !== undefined ? strMeal : strDrink }
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
